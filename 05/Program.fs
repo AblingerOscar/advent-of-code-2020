bf5df856ffde8bc8d3dc968c8dde023def8b5e13
Oscar Ablinger <dev.ablinger@gmail.com>
2020-12-05 20:54:53 +0100

    feat(05): implement day 5

---
+++ 05/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+
+let getData () = seq {
+    yield! System.IO.File.ReadLines "data/source.txt"
+}
+
+let getSeat seatDescriptor =
+    let rec recGetSeat descriptor rowMin rowMax columnMin columnMax =
+        let middle min max = (min + max + 1) / 2
+        match descriptor with
+        | 'B'::t -> recGetSeat t (middle rowMin rowMax) rowMax columnMin columnMax
+        | 'F'::t -> recGetSeat t rowMin (middle rowMin rowMax) columnMin columnMax
+        | 'R'::t -> recGetSeat t rowMin rowMax (middle columnMin columnMax) columnMax
+        | 'L'::t -> recGetSeat t rowMin rowMax columnMin (middle columnMin columnMax)
+        | _ -> rowMin, columnMin
+
+    recGetSeat seatDescriptor 0 127 0 7
+
+let calcSeatId (r, c) = r * 8 + c
+
+[<EntryPoint>]
+let main argv =
+    let seatIds =
+        getData ()
+        |> Seq.map (Seq.toList >> getSeat >> calcSeatId)
+        |> Seq.toList
+        |> List.sort
+
+    let maxId = List.last seatIds
+    printfn "The highest seat id is %d" maxId
+
+    let myId =
+        List.pairwise seatIds
+        |> List.find (fun (a, b) -> a + 1 <> b)
+        |> snd
+        |> fun a -> a - 1
+
+    printfn "My seat id is %d" myId
+
+    Lib.waitForUser ()
+    0 // return an integer exit code
