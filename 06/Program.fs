7fadc640f7403190bb4ef3c89984526f781868c3
Oscar Ablinger <dev.ablinger@gmail.com>
2020-12-06 11:16:00 +0100

    feat(06): implement day 6-2

--- 06/Program.fs
+++ 06/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 
+// part 1
 let getData () = seq {
     let lines = System.IO.File.ReadLines "data/source.txt"
 
@@ -18,11 +19,28 @@ let getData () = seq {
 
 let getCntOfDiffAnswers answers = (Set.ofSeq answers).Count
 
+// part 2
+let getData2 () = seq {
+    yield! Lib.readEmptyLineSeparatedSections "data/source.txt"
+}
+
+let getCntOfSameAnswers answers =
+    Seq.map Set.ofSeq answers
+    |> Set.intersectMany
+    |> Set.count
+
 [<EntryPoint>]
 let main argv =
+    // part 1
     getData ()
     |> Seq.map getCntOfDiffAnswers
     |> Seq.reduce (+)
-    |> printfn "Sum of counts: %d"
+    |> printfn "Sum of counts (any yes): %d"
+
+    // part 2
+    getData2 ()
+    |> Seq.map getCntOfSameAnswers
+    |> Seq.reduce (+)
+    |> printfn "Sum of counts (all yes): %d"
 
     0 // return an integer exit code
--- lib/Lib.fs
+++ lib/Lib.fs
@@ -2,6 +2,29 @@
 
 open System
 
+let splitBy f input =
+    let i = ref 0
+    input
+    |> Seq.groupBy (fun s -> (if f s then incr i) ; !i)
+    |> Seq.map snd
+
+let splitAt f input =
+    let i = ref 0
+    input
+    |> Seq.choose (fun s ->
+                    if f s then
+                        incr i
+                        None
+                    else
+                        Some (!i, s))
+    |> Seq.groupBy fst
+    |> Seq.map (snd >> Seq.map snd)
+
+let readEmptyLineSeparatedSections fileName =
+    System.IO.File.ReadLines fileName
+    |> splitAt ((=) "")
+    
+
 let waitForUser () =
     printfn "Finished. Press 'e' to exit the program…"
 
