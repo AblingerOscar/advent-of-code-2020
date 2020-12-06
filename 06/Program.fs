1d02e4ee88f609eb483feb56f99d898e6a31f224
Oscar Ablinger <dev.ablinger@gmail.com>
2020-12-06 10:46:45 +0100

    feat(06): implement day 6-1

---
+++ 06/Program.fs
@@ -0,0 +1,28 @@
+﻿open System
+
+let getData () = seq {
+    let lines = System.IO.File.ReadLines "data/source.txt"
+
+    let mutable result = ""
+    
+    for line in lines do
+        if String.IsNullOrEmpty line then
+            yield result
+            result <- ""
+        else
+            result <- result + line
+
+    if not (String.IsNullOrEmpty result) then
+        yield result
+}
+
+let getCntOfDiffAnswers answers = (Set.ofSeq answers).Count
+
+[<EntryPoint>]
+let main argv =
+    getData ()
+    |> Seq.map getCntOfDiffAnswers
+    |> Seq.reduce (+)
+    |> printfn "Sum of counts: %d"
+
+    0 // return an integer exit code
