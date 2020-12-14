33c76f9aee4b57ea8e8b571bd5da2307f82fc3f1
Oscar Ablinger <dev.ablinger@gmail.com>
2020-12-14 23:06:41 +0100

    fix(13): use sieving of chinese remainder theory

--- 13/Program.fs
+++ 13/Program.fs
@@ -61,13 +61,58 @@ let part2 argv =
     let (largestBusOffset, largestBusId) = getLargest interestingBuses
 
     // try all multiples of the largest bus - its offset (aka the starting timestamp)
-    Seq.initInfinite (fun i -> largestBusId * (int64 i) - int64 largestBusOffset)
+    let start = 100000000000000L - (100000000000000L % largestBusId)
+    Seq.initInfinite (fun i -> start + largestBusId * (int64 i) - int64 largestBusOffset)
     |> Seq.find isValid
     |> printfn "%d"
+
+
+let rec chineseRemainder (cs: (int64 * int64) list) (x: int64) (N: int64) =
+    match cs with
+    | [] -> Some(x)
+    | (a,n)::rest ->
+        let arrProgress = Seq.unfold (fun x -> Some(x, x+N)) x
+        let firstXmodNequalA = Seq.tryFind (fun x -> a = x % n)
+        match firstXmodNequalA (Seq.take (int n) arrProgress) with
+        | None -> None
+        | Some(x) -> chineseRemainder rest x (N*n)
+
+let part2b argv =
+    let rec posMod (n: int64) (m: int64) =
+        let r = (n % m)
+        if r < 0L then
+            posMod (n + m) m
+        else
+            r
+
+    let interestingBuses =
+        getData2 argv
+        |> Seq.indexed
+        |> Seq.map (fun (offset, bus) ->
+            match bus with
+            | X -> None
+            | Id id -> Some (int64 offset, int64 id))
+        |> Seq.choose id
+        |> Seq.map (fun (offset, id) -> (posMod (-offset) id), id)
+        |> List.ofSeq
+
+
+    let cs =
+        interestingBuses
+        |> List.map (fun (offset, id) -> (offset % id, id))
+        |> List.sortByDescending snd
+
+    match cs with
+    | (x, N)::t -> 
+        match chineseRemainder t x N with
+        | None -> printfn "invalid -> no solution"
+        | Some x -> printfn "%d" x
+    | _ -> printfn "empty list"
+
 
 [<EntryPoint>]
 let main argv =
     part1 argv
-    part2 argv
+    part2b argv
 
     0
